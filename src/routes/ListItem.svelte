5e3c13535403ef85e30d251c5d33623ba9915955
Geoffroy Warin <code@geowarin.com>
2024-01-22 14:11:44 +0100

    ListItem

---
+++ src/routes/ListItem.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let name: string;
+
+
+	let innerDiv: HTMLDivElement;
+	function mouseOver(e: MouseEvent) {
+		innerDiv.classList.remove('animate');
+		innerDiv.classList.add('animate');
+	}
+	function animationEnd(e: AnimationEvent) {
+		e.target.classList.remove('animate');
+	}
+
+</script>
+
+<li>
+	<div class="title" on:mouseover={mouseOver} role="listitem" on:focus={() => {}}>
+		<div>{name}</div>
+
+		<div class="title2" bind:this={innerDiv} style="pointer-events: none" on:animationend={animationEnd}>
+			<div>{name}</div>
+		</div>
+	</div>
+</li>
--- src/routes/Old.svelte
+++ src/routes/Old.svelte
@@ -1,26 +1,6 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-
-		document.querySelectorAll('.ranking li > .title').forEach((e => {
-			const t = e.appendChild(e.cloneNode(true)) as HTMLSpanElement;
-			t.classList.remove("title");
-			t.classList.add("title2");
-			t.style.pointerEvents = 'none';
-			// const t = e as HTMLDivElement;
-			e.addEventListener('mouseenter', (e => {
-				console.log('mouse enter!!!!!!!');
-				t.classList.remove('animate');
-				t.offsetWidth;
-				t.classList.add('animate');
-			}));
-			e.addEventListener('animationend', (e => {
-				console.log('animation end', e);
-				t.classList.remove('animate');
-			}));
-		}));
-	});
+	
+	import ListItem from './ListItem.svelte';
 
 	const titles = [
 		{
@@ -47,15 +27,7 @@
 <div class="ranking">
 	<ol>
 		{#each titles as title}
-			<li>
-				<div class="title">
-					<div>{title.name}</div>
-				</div>
-			</li>
+			<ListItem name={title.name}/>
 		{/each}
 	</ol>
 </div>
-
-<style>
-
-</style>
